c4f4a1fd7134039df6bfe6bc437c067a584df1e2
Yassine Aba <yassineaba.dev@gmail.com>
2026-03-07 00:56:23 +0000

    Update route.ts
    Removed custom GET function and replaced with handleAuth.

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,22 +1,3 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 export const GET = handleAuth();
 
-
-export async function GET() {
-  try {
-    const data = await db.query.documentTable.findMany();
-
-    return Response.json({
-      success: true,
-      data
-    });
-
-  } catch (error) {
-    console.error(error);
-
-    return Response.json({
-      success: false,
-      message: "Server Error"
-    }, { status: 500 });
-  }
-}
